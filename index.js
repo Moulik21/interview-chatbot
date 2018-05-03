d916611b492f4a2c7ae75ce715535bb86da2a16b
Moulik Gaglani <moulikgaglani@gmail.com>
2018-05-02 23:14:49 -0400

    added '/' route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+app.get('/', (req,res) => {
+  res.status(200).send("Hello world!");
+  
+});
 // use port 3000 unless there exists a preconfigured port
 var port = 3000 || process.env.PORT;
 
