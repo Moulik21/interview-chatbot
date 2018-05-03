8182efba5f2367190d16ac2b411d47e8c2c6c0ab
Moulik Gaglani <moulikgaglani@gmail.com>
2018-05-03 18:04:12 -0400

    fixing spelling error of method sendMessage

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -22,7 +22,7 @@ const apiAiClient = require('apiai')(API_AI_TOKEN);
 const FB_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const request = require('request');
 
-const sentMessage = (senderId, text) => {
+const sendMessage = (senderId, text) => {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: FB_ACCESS_TOKEN },
