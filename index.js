c89295d0b270a90a1ae43b3d94da99964269fa3e
Moulik Gaglani <moulikgaglani@gmail.com>
2018-05-02 23:29:03 -0400

    fixing heroku error

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.set('port', (process.env.PORT || 3000));
 
 app.get('/', (req,res) => {
   res.status(200).send("Hello world!");
-  
+
 });
 // use port 3000 unless there exists a preconfigured port
 var port = 3000 || process.env.PORT;
 
-app.listen(port, () => {
-  console.log('Webhook server is listening on port ' + port);
+app.listen(app.get('port'), () => {
+  console.log('Webhook server is listening on port ' + app.get('port'));
 })
