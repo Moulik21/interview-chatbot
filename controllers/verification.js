dd980ac75a1209ae3e1a3d5adb144e3c16827a4d
Moulik Gaglani <moulikgaglani@gmail.com>
2018-05-03 17:32:20 -0400

    added dialogflow integration

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   const hubChallenge = req.query['hub.challenge'];
 
   const hubMode = req.query['hub.mode'];
-  const verifyTokenMatches = (req.query['hub.verify_token'] === 'qYF6A10zBN0U');
+  const verifyTokenMatches = (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN);
 
   if (hubMode && verifyTokenMatches) {
     res.status(200).send(hubChallenge);
